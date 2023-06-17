3b7f9c7a366049e1d74100bc0b07d9dfec38092a
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-06-17 18:55:26 -0500

    3

---
+++ for/2.py
@@ -0,0 +1,11 @@
+num= int(input("Ingresa un numero: "))
+
+if num > 10:
+    resultado = num ** 10
+    print(resultado)
+else:
+    resultado = num + 3
+    print(resultado)
+
+for i in range(num):
+    print(i)
